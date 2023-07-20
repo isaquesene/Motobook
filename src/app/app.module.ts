1e48ccc5836cf10faf50c14e4fdc676b088140ba
isaque sene <isaqueconcursos26@gmail.com>
2023-07-20 17:45:43 -0300

    feat: lazy-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    /* HomeModule */
   ],
   providers: [],
   bootstrap: [AppComponent]
